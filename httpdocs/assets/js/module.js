83d44a32db83eccd3b4baf38dbb86769671ef749
iLexN <ilex.job@gmail.com>
2016-07-06 16:16:28 +0800

    js improve

--- httpdocs/assets/js/module.js
+++ httpdocs/assets/js/module.js
@@ -33,87 +33,16 @@ var passwordModule = (function () {
     };
 })();
 
-// control dom flow ?
-var SignUpControl = (function () {
-    var $jsPassInfo, $btn, $length, $letter, $num, $passSame;
-    var validCssClass, inValidCssClass;
-
-    function setting(obj) {
-        $jsPassInfo = obj.jsPassInfo;
-        $btn = obj.btn;
-        $length = obj.length;
-        $letter = obj.letter;
-        $num = obj.num;
-        $passSame = obj.passSame;
-        validCssClass = obj.validClass;
-        inValidCssClass = obj.invalidClass;
-    }
-    function setP1() {
-        passwordModule.setP1($(this).val());
-        check();
-    }
-    function setP2() {
-        passwordModule.setP2($(this).val());
-        check();
-    }
-    function valid($obj) {
-        $obj.removeClass(inValidCssClass).addClass(validCssClass);
-    }
-    function inValid($obj) {
-        $obj.removeClass(validCssClass).addClass(inValidCssClass);
-    }
-    function check() {
-        if (passwordModule.isLenghtPass(8)) {
-            valid($length);
-        } else {
-            inValid($length);
-        }
-        if (passwordModule.hasUpperCase() && passwordModule.hasLowerCase()) {
-            valid($letter);
-        } else {
-            inValid($letter);
-        }
-        if (passwordModule.hasNumber()) {
-            valid($num);
-        } else {
-            inValid($num);
-        }
-        if (passwordModule.isPassSame()) {
-            valid($passSame);
-        } else {
-            inValid($passSame);
-        }
-        if ($jsPassInfo.find('li').hasClass('invalid') === false) {
-            $btn.prop("disabled", false);
-        } else {
-            $btn.prop("disabled", true);
-        }
-    }
-    function infoShow() {
-        $jsPassInfo.show();
-    }
-    function infoHide() {
-        $jsPassInfo.hide();
-    }
-    return {
-        setting: setting,
-        setP1: setP1,
-        setP2: setP2,
-        infoShow: infoShow,
-        infoHide: infoHide
-    };
-})();
-
 //helper
 $.extend({
     jshook: function (hookName) {
         var selector;
         if (!hookName || hookName === '*') {
             // select all data-hooks
-            selector = '[data-hook]';
+            selector = '[data-jshook]';
         } else {
             // select specific data-hook
-            selector = '[data-hook~="' + hookName + '"]';
+            selector = '[data-jshook~="' + hookName + '"]';
         }
         return $(selector);
     }
