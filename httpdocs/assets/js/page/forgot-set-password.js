1ec0320bbf32eca8a4ce6346d542cca32f905c07
iLexN <ilex.job@gmail.com>
2017-01-04 16:46:07 +0800

    template change name , add 2 col template

---
+++ httpdocs/assets/js/page/forgot-set-password.js
@@ -0,0 +1,51 @@
+//set forgot password
+(function () {
+    var $form = $.jshook('setforgotPasswordForm');
+    var $btn = $.jshook('setforgotpasswordBtn');
+    var $msg = $.jshook('setForgotPasswordMsg');
+    var $success = $.jshook('setforgotPasswordSuccess');
+
+    function submit() {
+        if (!isOnLine.check()) {
+            return false;
+        }
+        var data = (csrf.getFormObj($form.serializeArray()));
+        $.ajax({
+            beforeSend: function () {
+                ajaxStart();
+            },
+            url: '/ajax/system/forgot-set-password',
+            type: 'POST',
+            data: data,
+            dataType: "json"
+        }).done(function (data, textStatus, jqXHR) {
+            if (data.status_code === 2570) {
+                //success
+                window.location.replace("/main");
+                return;
+            }
+            //fail
+            ajaxEnd(data.errors.title);
+        }).fail(function (jqXHR, textStatus, errorThrown) {
+            ajaxEnd(textStatus);
+        });
+    }
+    function ajaxStart() {
+        $btn.prop("disabled", true);
+        $msg.html('').parent().addClass('hide');
+    }
+    function ajaxEnd(msg) {
+        $msg.html(msg).parent().removeClass('hide');
+        loadingBox.close();
+        $btn.prop("disabled", false);
+    }
+    function init(){
+        $btn.on({
+            'click.setforgotpass': submit
+        });
+        $form.on({
+            submit : false
+        });
+    }
+    init();
+})();
