8322aa1ccd05a1a1f6a621dff4219aa3c788a0fe
iLexN <iLexN@users.noreply.github.com>
2017-03-30 11:06:23 +0800

    add zip file download (#20)
    * add zip file download
    
    * cs fix
    
    * ci fix

---
+++ app/src/Controller/Page/ZipFileDownload.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace PP\WebPortal\Controller\Page;
+
+use PP\WebPortal\AbstractClass\AbstractContainer;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+final class ZipFileDownload extends AbstractContainer
+{
+    /**
+     * Login-ed Page.
+     *
+     * @param ServerRequestInterface $request
+     * @param ResponseInterface      $response
+     * @param array                  $args
+     *
+     * @return ResponseInterface
+     */
+    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, array $args)
+    {
+        $filename = 'download.zip';
+
+        $file_path = $args['id'].'/'.$filename;
+
+        $filesystem = $this->helper->getFileSystem($this->c->get('policyFileConfig')['path']);
+
+        if ($filesystem->has($file_path)) {
+            return $this->helper->sendFile($response, $this->c->get('policyFileConfig')['path'].'/'.$file_path, $filename);
+        }
+
+        $fileResponse = $this->downloadFromAPI($args['id']);
+        $filesystem->write($file_path, $fileResponse->getBody());
+
+        return $this->helper->sendFile($response, $this->c->get('policyFileConfig')['path'].'/'.$file_path, $filename);
+    }
+
+    private function downloadFromAPI($id)
+    {
+        $response = $this->c['httpClient']->request('GET', 'user-policy/'.$id.'/zip');
+
+        return $response;
+    }
+}
